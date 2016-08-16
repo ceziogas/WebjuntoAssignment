14233733b2b86c1e9b09cf8e6760a63abc50c72f
Constantine Ziogas <cez27@drexel.edu>
2016-08-16 16:22:34 -0400

    Google Maps working using ionic serve with localhost

--- testapp/www/js/controllers.js
+++ testapp/www/js/controllers.js
@@ -17,10 +17,30 @@ angular.module('starter.controllers', ['ionic' , 'ngCordova'])
  
     $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
  
+	google.maps.event.addListenerOnce($scope.map, 'idle', function(){
+ 
+	var marker = new google.maps.Marker({
+		map: $scope.map,
+		animation: google.maps.Animation.DROP,
+		position: latLng
+	});      
+ 
+	var infoWindow = new google.maps.InfoWindow({
+		content: "This is where you are located!"
+	});
+ 
+	google.maps.event.addListener(marker, 'click', function () {
+		infoWindow.open($scope.map, marker);
+	});
+ 
+});    
+ 
+});
+ 
   }, function(error){
     console.log("Could not get location");
   })
-})
+
 
 
 
